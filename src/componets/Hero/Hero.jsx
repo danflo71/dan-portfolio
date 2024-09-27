b4cdc60cb384e81ae7afbe84079fbe24809a5137
danflo71 <132175672+danflo71@users.noreply.github.com>
2024-09-27 01:14:35 -0400

    fix
    fix

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
      <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Dan </h1>
         <p className={styles.description}>
-          I'm a Computer & Electrical Engineer in my 4th year of university with experience building embeded systems to front and backend development.
+          I'm a Electrical & Computer Engineer in my 4th year of university with experience building embeded systems to front and backend development.
           Reach out to me if you would like to learn more! 
         </p>
         <a href = "mailto:danflores6025@gmail.com" className={styles.contactBtn}>Contact Me</a>
